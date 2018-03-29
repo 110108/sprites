70a05747cfe140d4ed08ae513e0f8ce9d0559e3a
110108 <31996737+110108@users.noreply.github.com>
2018-03-29 12:47:41 -0400

    3/29/18

--- Assets/reset.cs
+++ Assets/reset.cs
@@ -3,14 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class reset : MonoBehaviour {
+	private Vector3 resetPos;
+	public GameObject player;
+	public int depth;
 
 	// Use this for initialization
 	void Start () {
-		
+		resetPos = new Vector3 (transform.position.x, transform.position.y+2, transform.position.z);
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		
+		if (player.transform.position.y<depth){
+			player.transform.position = resetPos;
+		}
 	}
 }
