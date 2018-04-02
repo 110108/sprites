c6c39e4032f0a28069ad332572046bee1e959f6d
110108 <31996737+110108@users.noreply.github.com>
2018-04-02 13:31:20 -0400

    4/2/2018

--- Assets/reset.cs
+++ Assets/reset.cs
@@ -5,17 +5,22 @@ using UnityEngine;
 public class reset : MonoBehaviour {
 	private Vector3 resetPos;
 	public GameObject player;
+	public GameObject other;
 	public int depth;
 
 	// Use this for initialization
 	void Start () {
 		resetPos = new Vector3 (transform.position.x, transform.position.y+2, transform.position.z);
+		other.SetActive (false);
 	}
 
 	// Update is called once per frame
 	void Update () {
 		if (player.transform.position.y<depth){
 			player.transform.position = resetPos;
+		}
+		if (player.transform.position.y>=16.00f&&player.transform.position.x<=7){
+			other.SetActive (true);
 		}
 	}
 }
---
+++ Assets/wall_jump.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wall_jump : MonoBehaviour {
+	
+	public Rigidbody2D rb;
+	public int jump;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody2D>();
+	}
+
+	void FixedUpdate()
+	{
+//		if (rb.velocity.y == 0) {
+			if (Input.GetButtonDown ("Jump")) {
+				rb.velocity = new Vector2 (0, jump);
+//			}
+		}
+	}
+}
