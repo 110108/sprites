4c399b8fcd20cf53fc65e699021c3be6dfb13093
110108 <31996737+110108@users.noreply.github.com>
2018-04-06 13:18:40 -0400

    This Is A Finished Game

---
+++ Assets/start.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class start : MonoBehaviour {
+	public Scene scene;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnMouseOver()
+	{
+		//If your mouse hovers over the GameObject with the script attached, do this
+		if (Input.GetMouseButtonDown(0)){
+			//SceneManager.SetActiveScene ("ssb");
+			SceneManager.LoadScene("ssb", LoadSceneMode.Single);
+		}
+	}
+}
